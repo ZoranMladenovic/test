1fd57a9461aedfedcb912a910a3753a4d7ee0a4d
ZoranMladenovic <>
2019-11-20 22:33:16 +0100

    izmena

--- proba/Procedures/dbo.izvCombo_Produkti.sql
+++ proba/Procedures/dbo.izvCombo_Produkti.sql
@@ -3,7 +3,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE procedure [dbo].[izvCombo_Produkti] @GrupaProdukta varchar(1000)
 as
-
+-- komentar 2
 select dimProdukt, case a.poreklo when 1 then 'KOOP - ' when 2 then 'KUP - ' else '' end + ProduktName  ProduktName 
 from dimProdukti a inner JOIN STRING_SPLIT(@GrupaProdukta,',') b on a.Nadredjena=b.value
 WHERE Nivo=2
